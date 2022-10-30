d5ce2f994d666c516d8c9c47ce1123fb3a285506
AK <akrivit@gmail.com>
2022-10-30 10:03:21 -0400

    fix linter; add athena dep

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-export default {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	root: true,
-  };
