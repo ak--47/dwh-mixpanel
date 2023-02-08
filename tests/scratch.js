0282ff1e1eb6a8c15955a0cc8e0fb030251c5c53
AK <akrivit@gmail.com>
2023-02-08 00:01:15 -0500

    add scratch file to tests

---
+++ tests/scratch.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ * THIS IS JUST A SCRATCH FILE 
+ * TO TEST RANDOM CONFIGS LOCALLY
+ * YOU PROBABLY DON'T NEED THIS 
+ * BUT I DID... ;) 
+ */
+
+/* eslint-disable no-undef */
+/* eslint-disable no-debugger */
+/* eslint-disable no-unused-vars */
+/* cSpell:disable */
+import main from "../index.js"
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+// link to a config!
+const current = require("../environments/current.json");
+
+// do the thing!
+main(current)
+	.then(result => {
+		//noop
+		debugger;
+	})
+	.catch(e => {
+		debugger;
+	}) 
