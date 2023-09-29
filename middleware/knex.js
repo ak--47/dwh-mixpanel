303df355a80c839dcc11ff1958c7420ce6865a9c
AK <akrivit@gmail.com>
2023-09-28 22:21:19 -0400

    upgrade deps

---
+++ middleware/knex.js
@@ -0,0 +1,20 @@
+// https://www.npmjs.com/package/knex
+// import knex from 'knex';
+
+// https://knexjs.org/guide/interfaces.html#streams
+// https://knexjs.org/guide/raw.html#raw-queries
+// https://chat.openai.com/share/8d809bb3-9f31-4ab7-8553-0b44347503e7
+
+/**
+ * @type {Knex}
+ */
+// const database = knex({
+// 	client: 'mysql',
+// 	connection: {
+// 		host: '127.0.0.1',
+// 		user: 'your_database_user',
+// 		password: 'your_database_password',
+// 		database: 'myapp_test',
+// 	},
+// });
+// database.migrate.latest();
